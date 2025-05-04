265237bd52748ab484e337ebb2b1990788527649
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-05-04 18:29:34 +0100

    Integrate next-intl for internationalization and update Next.js configuration
    - Added next-intl plugin to enable internationalization support.
    - Updated next.config.ts to use the next-intl plugin.
    - Modified package.json to include next-intl and @next/bundle-analyzer dependencies.
    - Refactored landing page to utilize translations from next-intl, simplifying the component structure.

---
+++ devNotes.md
@@ -0,0 +1,14 @@
+# Developer notes
+- when remotely fetching data use tanstack-query or swr.vercel.app or maybe even native nextjs fetch
+- for forms use formik or react-hook-form and zod
+- use cypress for e2e tests
+- react-icons
+- react-big-calendar
+- paraglide-next-i18n
+- github.com/garmeeh/next-seo
+debugbear.com/blog/nextjs-performance
+
+
+
+## plans
+- checkout with stripe (docs.stripe.com/sdks/stripekjs-react)
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import createNextIntlPlugin from 'next-intl/plugin'
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+
+
+// export const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+
+
+
+export default withNextIntl(nextConfig);
